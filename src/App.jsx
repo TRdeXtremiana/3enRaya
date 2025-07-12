50648c138a7f9151764fe26657cf280225cb0b1c
Diego <dextremiana1998@gmail.com>
2025-07-12 14:25:32 +0200

    Actualizar diseño de cuadricula

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import Header from "./components/Header";
 import Game from "./components/Game";
 import Footer from "./components/Footer";
-import styles from "./components/App.module.css";
 
 export default function App() {
   return (
-    <div className={styles.app}>
+    <>
       <Header />
-      <main>
-        <Game />
-      </main>
+      <Game />
       <Footer />
-    </div>
+    </>
   );
 }
